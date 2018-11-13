e42d65ac8b5b2b7924e4ce63bca74c27e1309096
fahmisalman <fahmisalman@gmail.com>
2018-11-13 17:28:23 +0700

    Fix directory

---
+++ FuzzyLogic/FIS.py
@@ -0,0 +1,66 @@
+class FIS(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.fuzzification = []
+
+    def tri(self, x, a):
+        if x <= a[0]:
+            return 0
+        elif a[0] < x < a[1]:
+            return (x - a[0]) / (a[1] - a[0])
+        elif x == a[1]:
+            return 1
+        elif a[1] < x < a[2]:
+            return (a[2] - x) / (a[2] - a[1])
+        else:
+            return 0
+
+    def trap(self, x, a):
+        if x <= a[0]:
+            return 0
+        elif a[0] < x < a[1]:
+            return (x - a[0]) / (a[1] - a[0])
+        elif a[1] <= x <= a[2]:
+            return 1
+        elif a[2] < x < a[3]:
+            return (a[3] - x) / (a[3] - a[2])
+        else:
+            return 0
+
+    def add(self, type, a, s):
+        self.fuzzification.append([type, a, s])
+
+    def rule(self, s, x):
+        a = []
+        for member in self.fuzzification:
+            if member[2] == s:
+                a = member
+        if a[0] == 'tri':
+            return self.tri(x, a[1])
+        elif a[0] == 'trap':
+            return self.trap(x, a[1])
+
+
+def inference(x1, x2):
+    return min(x1, x2)
+
+
+if __name__ == '__main__':
+
+    suhu = FIS('Suhu')
+    suhu.add('tri', [1, 2, 3], 'rendah')
+    suhu.add('tri', [2, 3, 4], 'sedang')
+    suhu.add('tri', [3, 4, 5], 'tinggi')
+
+    kelembaban = FIS('Kelembaban')
+    kelembaban.add('tri', [1, 2, 3], 'rendah')
+    kelembaban.add('tri', [2, 3, 4], 'sedang')
+    kelembaban.add('tri', [3, 4, 5], 'tinggi')
+
+    ya = []
+    tidak = []
+
+    ya.append(inference(suhu.rule('rendah', 2), kelembaban.rule('rendah', 2)))
+
+
---
+++ NaiveBayes/MultinomialNaiveBayes/MultinomialNB.py
@@ -0,0 +1,68 @@
+import numpy as np
+import math
+import os
+
+os.chdir('../..')
+
+
+class MultinomialNB():
+
+    def __init__(self):
+        self.prior = []
+        self.likelihood = []
+        self.list_label = []
+
+    def load_dataset(self, data):
+        temp = np.loadtxt(open("%s/Sample-Datasets/%s.csv" % (os.path.abspath(os.curdir), data), "r"), delimiter=",")
+        return temp[:, 0:2], temp[:, 2]
+
+    def prior_probability(self, y, a):
+        prior = []
+        for i in range(len(a)):
+            temp = 0
+            for j in range(len(y)):
+                if a[i] == y[j]:
+                    temp += 1
+            temp += 1
+            prior.append(temp)
+        for i in range(len(prior)):
+            prior[i] /= len(y)
+        return y
+
+    def likelihood_probability(self, x, y, a):
+        l = []
+        for i in range(len(a)):
+            temp = []
+            for j in range(len(y)):
+                if y[j] == a[i]:
+                    temp.append(x[j])
+            l.append(sum(temp) + 1)
+        return l
+
+    def fit(self, data, label):
+
+        self.list_label = list(set(label))
+
+        self.prior = self.prior_probability(label, self.list_label)
+
+        for i in range(len(data[0])):
+            self.likelihood.append(self.likelihood_probability(np.array(data)[:, i], label, self.list_label))
+
+        self.likelihood = np.array(self.likelihood)
+
+        total = []
+        for i in range(len(self.likelihood[0])):
+            total.append(sum(self.likelihood[:, i]))
+
+        for i in range(len(self.likelihood)):
+            for j in range(len(self.likelihood[i])):
+                self.likelihood[i][j] /= total[j]
+
+    def predict(self, data):
+        temp = [0] * len(self.list_label)
+        for j in range(len(data)):
+            for k in range(len(self.list_label)):
+                temp[k] += math.log(self.likelihood[j][k] ** data[j])
+        for i in range(len(self.list_label)):
+            temp[i] += math.log(self.prior[i])
+        return self.list_label[temp.index(max(temp))]
